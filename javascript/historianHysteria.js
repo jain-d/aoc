6b19893a161b02b1ec9f209f0bb3c1abbef91658
jain-d <jain.divyansh.0017@gmail.com>
2024-12-02 01:47:23 +0530

    Part Two

--- javascript/historianHysteria.js
+++ javascript/historianHysteria.js
@@ -1,13 +1,3 @@
-import {c} from "../../javascript/colors.js";
-
-let smallInput = 
-`3   4
-4   3
-2   5
-1   3
-3   9
-3   3`;
-
 let leftList = [];
 let rightList = [];
 let summation = 0;
@@ -15,6 +5,7 @@ let contents = Bun.file("../input/day1.txt");
 let fileData = await contents.text();
 let arrayedFileData = fileData.trim().split("\n");
 
+// Part One
 for (let entry of arrayedFileData) {
    leftList.push(entry.split("   ")[0]);
    rightList.push(entry.split("   ")[1]);
@@ -26,3 +17,27 @@ for (let i = 0; i < leftList.length; i++) {
    summation += (Math.abs(leftList[i] - rightList[i]));
 }
 console.log(summation);
+
+// Part Two
+let finalSummation = 0;
+let lastCount = 0;
+for (let i = 0; i < leftList.length; i++) {
+   let count = 0;
+   if (i > 0 && leftList[i] === leftList[i - 1]) {
+      finalSummation += lastCount;
+      continue;
+   }
+   let startingIndex = rightList.indexOf(leftList[i]);
+   if (startingIndex >= 0) {
+      for (let j = startingIndex; j < rightList.length; j++) {
+         if (rightList[j] > leftList[i]) {
+            break;
+         } else {
+            count++;
+         }
+      }
+   }
+   finalSummation += (count * +(leftList[i]));
+   lastCount = (count * +(leftList[i]));
+}
+console.log(finalSummation);
