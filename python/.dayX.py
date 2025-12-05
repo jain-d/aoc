b4eca02fdf1e017927013f2d5a328e006a34f818
jain-d <jain.divyansh.0017@gmail.com>
2025-12-05 14:24:12 +0530

    --- improve setup ---

--- python/.dayX.py
+++ python/.dayX.py
@@ -5,6 +5,6 @@ import sys
 file_path = "../input/dayX.txt" if len(sys.argv) > 1 else "./.temp.txt"
 
 with open(file_path) as file:
-    raw_data = file.read().splitlines()
+    data = file.read().splitlines()
 
 
